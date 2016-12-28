02b874b99d3874c3ebfb2bc915cd41d6b5545740
cketti <ck@cketti.de>
2016-12-28 16:02:37 +0100

    Update config for 33C3

--- app/src/main/java/org/c_base/ampelcontrol/TrafficLight.java
+++ app/src/main/java/org/c_base/ampelcontrol/TrafficLight.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 
 public class TrafficLight {
-    private static final TrafficLight AMPEL_1 = new TrafficLight(R.string.ampel_1_name, "151.217.64.87");
-//    private static final TrafficLight AMPEL_2 = new TrafficLight(R.string.ampel_2_name, "151.217.43.28");
-//    private static final TrafficLight AMPEL_3 = new TrafficLight(R.string.ampel_3_name, "151.217.95.186");
+    private static final TrafficLight AMPEL_1 = new TrafficLight(R.string.ampel_1_name, "alberich.c3kid.space");
+    private static final TrafficLight AMPEL_2 = new TrafficLight(R.string.ampel_2_name, "brunhilde.c3kid.space");
+    private static final TrafficLight AMPEL_3 = new TrafficLight(R.string.ampel_3_name, "sigfried.c3kid.space");
 
     public static final List<TrafficLight> LIGHTS = Collections.unmodifiableList(
-            Arrays.asList(AMPEL_1));
+            Arrays.asList(AMPEL_1, AMPEL_2, AMPEL_3));
 
     private final int nameResId;
     private final String host;
